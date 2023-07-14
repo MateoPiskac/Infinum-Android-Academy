ac79e711e12be0c7a889466c61b10b3bc47e1373
Mateo Piskac <mateopiskac2@gmail.com>
2023-07-14 21:35:52 +0200

    Amend code to fit 2nd assignment notes from pull request

--- app/src/main/java/infinuma/android/shows/ui/LoginActivity.kt
+++ app/src/main/java/infinuma/android/shows/ui/LoginActivity.kt
@@ -1,7 +1,6 @@
 package infinuma.android.shows.ui
 
 import  android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -12,46 +11,9 @@ import androidx.core.content.ContextCompat
 import infinuma.android.shows.R
 import infinuma.android.shows.databinding.ActivityLoginBinding
 
-/*
-    Activity Lifecycle
-    onCreate() – called when the activity is first created.
-    onStart() – called when the activity is displayed to the user.
-    onResume() – called when the activity is displayed to the user and is ready to receive user input.
-    onPause() – called when the activity loses focus but is still visible to the user.
-    onStop() – called when the activity is no longer visible to the user.
-    onRestart() – called when the activity has been stopped and is starting again.
-    onDestroy() – called when activity is destroyed.
-
-    When the app is created the following methods are called:
-    onCreate()
-    onStart()
-    onResume()
-
-    When moving the app into the background the following methods are called:
-    onPause()
-    onStop()
-    When moving the app back to the foreground the following methods are called:
-    onRestart()
-    onStart()
-    onResume()
-
-    When locking the device the following methods are called:
-    onPause()
-    onStop()
-    When unlocking the device the following methods are called:
-    onRestart()
-    onStart()
-    onResume()
-
-    When killing the app the following methods are called:
-    onPause()
-    onStop()
-    onDestroy()
-
- */
+const val USERNAME = "username"
 class LoginActivity : AppCompatActivity() {
-
-    val USERNAME = "username"
+    private val emailRegex : Regex = Regex("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
     private lateinit var binding: ActivityLoginBinding
     private val watcher = object : TextWatcher {
         override fun beforeTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) {
@@ -60,7 +22,7 @@ class LoginActivity : AppCompatActivity() {
 
         override fun onTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) {
             binding.emailInputField.error = null
-            if (binding.emailInputField.text?.matches(Regex("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")) == true && (binding.passwordInputField.text?.length
+            if (binding.emailInputField.text?.matches(emailRegex) == true && (binding.passwordInputField.text?.length
                     ?: 0) >= 6
             ) {
                binding.emailInputField.error = null
@@ -68,7 +30,7 @@ class LoginActivity : AppCompatActivity() {
                 binding.loginButton.setTextColor(ContextCompat.getColor(this@LoginActivity, R.color.purple))
 
             } else {
-                if(binding.emailInputField.text?.matches(Regex("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")) == false)
+                if(binding.emailInputField.text?.matches(emailRegex) == false)
                     binding.emailInputField.error = "Invalid Email"
                 binding.loginButton.isEnabled = false
                 binding.loginButton.setTextColor(ContextCompat.getColor(this@LoginActivity, R.color.white))
@@ -96,33 +58,4 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("LoginActivity", "onStart is called")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("LoginActivity", "onRestart is called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("LoginActivity", "onResume is called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("LoginActivity", "onPause is called")
-    }
-
-    override fun onStop() {
-        Log.d("LoginActivity", "onStop is called")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d("LoginActivity", "onDestroy is called")
-        super.onDestroy()
-    }
 }
